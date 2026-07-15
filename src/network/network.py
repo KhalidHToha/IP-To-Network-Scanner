8feb89c10e1151fc22b2ddd9545229b2b04bae8e
Khalid Hasan Toha <79449426+KhalidHToha@users.noreply.github.com>
2026-07-15 20:03:56 +0600

    feat(network): add local IP detection

--- src/network/network.py
+++ src/network/network.py
@@ -1,11 +1,15 @@
 import socket
 
 
-def get_local_ip():
-    """Return the local IPv4 address."""
+def get_local_ip() -> str:
+    """
+    Returns the local IPv4 address.
+    """
+
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.connect(("8.8.8.8", 80))
-            return s.getsockname()[0]
-    except Exception:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.connect(("8.8.8.8", 80))
+            return sock.getsockname()[0]
+
+    except OSError:
         return "127.0.0.1"
